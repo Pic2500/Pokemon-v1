630411b190ed1c935267d400c1a9804b29f33cd0
Domagoj <pintar.domagoj@gmail.com>
2025-01-02 12:54:35 +0100

    Fireball

---
+++ Data/attacks.js
@@ -0,0 +1,22 @@
+const attacks = {
+  Tackle: {
+    name: "Tackle",
+    damage: 10,
+    type: "Normal",
+  },
+  Fireball: {
+    name: "Fireball",
+    damage: 35,
+    type: "Fire",
+  },
+  Scratch: {
+    name: "Scratch",
+    damage: 0,
+    type: "Normal",
+  },
+  FireRay: {
+    name: "FireRay",
+    damage: 20,
+    type: "Fire",
+  },
+};
--- PokemonCanvas.js
+++ PokemonCanvas.js
@@ -368,24 +368,27 @@ const emby = new Sprite({
   },
   animate: true,
 });
+
+const renderedSprites = [draggle, emby];
 function animateBattle() {
   window.requestAnimationFrame(animateBattle);
   battleBackground.draw();
-  draggle.draw();
-  emby.draw();
+
+  renderedSprites.forEach((sprite) => {
+    sprite.draw();
+  });
 }
 //animate();
 animateBattle();
 
+//event listeners za napade
 document.querySelectorAll("button").forEach((button) => {
-  button.addEventListener("click", () => {
+  button.addEventListener("click", (e) => {
+    const selectedAttack = attacks[e.currentTarget.innerHTML];
     emby.attack({
-      attack: {
-        name: "Tackle",
-        damage: 10,
-        type: "Normal",
-      },
+      attack: selectedAttack,
       recipient: draggle,
+      renderedSprites,
     });
   });
 });
